220f8269f98587241ef665ff4b85932286cf2f16
Vanesa Edo <vanesa.en@gmail.com>
2024-02-21 23:20:15 +0100

    Realizada la primera card en modo pre-2015.

--- script.js
+++ script.js
@@ -88,6 +88,7 @@ document.getElementById("conenedor") = parent;
 
 //Parte 2
 /*
+
 let card = "<div>"
 
 for (j=0; j < books.length ; j++){
@@ -110,75 +111,159 @@ for (j=0; j < books.length ; j++){
 }
 */
 
-
-
-
-
 //Imagen
-
+//Creo el objeto de DOM img
 let imag = document.createElement("img");
+
+//Creo el objeto de DOM atributo src
 let atrib = document.createAttribute("src");
+
+//Indico dónde voy a incluir estos objetos, y con ello se pintan. Sin esto no se verían, existirían pero en modo virtual.
 const section = document.getElementsByTagName("section")[0];
 
+//Ahora que están pintados creo una variable con el valor para el atributo src
 let atribValue = books[0].imageLink;
+
+//Incluyo en el atributo src (atrib) el valor cogido, mediante la variable anterior, del objeto.
 atrib.value = atribValue;
+
+//Introduzco y con ello pinto el atributo src ya con su valor dentro de la etiqueta img.
 imag.setAttributeNode(atrib);
 
+//Introduzco y con ello pinto la la etiqueta imagen (imag) en el primer elemento section (const section)
 section.appendChild(imag);
 
 //Link
+
+//Creo el elemento de DOM etiqueta <a>
 let anchor = document.createElement("a");
+
+//Creo el elemento de DOM atributo href
 let href = document.createAttribute("href");
 
-
+//No necesito aludir a section porque con la vez ha sido suficiente
+//Almaceno el valor que cojo del objeto en la variable hrefValue
 let hrefValue = books[0].link;
+
+//Incorporo el valor del atributo al atributo (nombrevarObjetoDOM.value = variable) 
 href.value = hrefValue;
+
+//Añado el atributo href al anchor.
 anchor.setAttributeNode(href);
 
+//Creo un texto para el enlace donde haremos click
+let textoLink = document.createTextNode("Más info aquì")
+//Añado el texto del lilnk entre las etiquetas <a>.
+anchor.appendChild(textoLink)
+
+//Incluyo el enlace a la sección
 section.appendChild(anchor);
 
-//Autor
-let h5 = document.createAttribute("h5");
-let tituloAutor = document.createTextNode("Autor: ");
-h5.appendChild(tituloAutor);
+//Titulo autor 
 
-let parrAutor = document.createElement("p");
-let textoParrAutor = document.createTextNode(books[0].author);
-parrAutor.appendChild(textoParrAutor);
+//Creo el elemento del DOM
+let h5 = document.createElement("h5");
+//Creo el texto que mostrará el h5
+let titAutor = document.createTextNode("Autor: ");
+//Añado el texto al h5
+h5.appendChild(titAutor)
+//Añado el h5 al section
+section.appendChild(h5)
+
+//Info de autor
+
+//Creo el elemento del DOM
+let pAutor = document.createElement("p");
+//Almaceno el dato a incluir cogiéndolo del objeto (books[0].title)
+let dataAutor = document.createTextNode(books[0].title);
+//Añado el texto al h5
+pAutor.appendChild(dataAutor);
+section.appendChild(pAutor); 
+
+//COUNTRY
+
+//Titulo country
+
+//Creo el elemento del DOM
+let h5Country = document.createElement("h5");
+//Creo el texto que mostrará el h5
+let titCountry = document.createTextNode("Country: ");
+//Añado el texto al h5
+h5Country.appendChild(titCountry)
+//Añado el h5 al section
+section.appendChild(h5Country)
+
+//Info de country
+
+//Creo el elemento del DOM
+let pCountry = document.createElement("p");
+//Almaceno el dato a incluir cogiéndolo del objeto (books[0].title)
+let dataCountry = document.createTextNode(books[0].country);
+//Añado el texto al h5
+pCountry.appendChild(dataCountry);
+section.appendChild(pCountry); 
+
+//LANGUAGE
+
+//Titulo language 
+
+//Creo el elemento del DOM
+let h5Lan = document.createElement("h5");
+//Creo el texto que mostrará el h5
+let titLan = document.createTextNode("Language: ");
+//Añado el texto al h5
+h5Lan.appendChild(titLan)
+//Añado el h5 al section
+section.appendChild(h5Lan)
+
+//Data Languages
+
+//Creo el elemento del DOM
+let pLan = document.createElement("p");
+//Almaceno el dato a incluir cogiéndolo del objeto (books[0].title)
+let dataLan = document.createTextNode(books[0].language);
+//Añado el texto al h5
+pLan.appendChild(dataLan);
+section.appendChild(pLan); 
+
+//PAGES
+
+//Creo el elemento del DOM
+let h5Pag = document.createElement("h5");
+//Creo el texto que mostrará el h5
+let titPag = document.createTextNode("Pages: ");
+//Añado el texto al h5
+h5Pag.appendChild(titPag)
+//Añado el h5 al section
+section.appendChild(h5Pag)
+
+//Info de pages
+
+//Creo el elemento del DOM
+let pPag = document.createElement("p");
+//Almaceno el dato a incluir cogiéndolo del objeto (books[0].title)
+let dataPag = document.createTextNode(books[0].pages);
+//Añado el texto al h5
+pPag.appendChild(dataPag);
+section.appendChild(pPag);
 
 
-//Titulo
-//Country
-//Language
-//pages
-//year
+//YEAR
 
+//Creo el elemento del DOM
+let h5Year = document.createElement("h5");
+//Creo el texto que mostrará el h5
+let titYear= document.createTextNode("Year: ");
+//Añado el texto al h5
+h5Year.appendChild(titYear)
+//Añado el h5 al section
+section.appendChild(h5Year)
 
-"author": "Unknown",
-"country": "Achaemenid Empire",
-"imageLink": "images/miro_impresionista.png",
-"language": "Hebrew",
-"link": "https://en.wikipedia.org/wiki/Book_of_Job\n",
-"pages": 176,
-"title": "The Book Of Job",
-"year": -600
-/*
+//Creo el elemento del DOM
+let pYear = document.createElement("p");
+//Almaceno el dato a incluir cogiéndolo del objeto (books[0].title)
+let dataYear = document.createTextNode(books[0].year);
+//Añado el texto al h5
+pYear.appendChild(dataYear);
+section.appendChild(pYear); 
 
-const para = document.createElement("p");
-para.innerText = "This is a paragraph";
-document.body.appendChild(para);
-
-const para2 = document.createElement("h1");
-para2.innerText = "Author";
-document.body.appendChild(para2);
-
-const para = document.createElement("p");
-para.innerText =  "";
-document.body.appendChild(para);
-
-const node = document.createElement("img");
-const imgnode = document.createTextNode(books[0].imageLink);
-node.appendChild(imgnode);
-document.getElementById("contenedor").appendChild(node);
-
-*/a
